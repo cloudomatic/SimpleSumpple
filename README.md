f2dc9f2d076206feb6b7da2a46b9ce0fe56d5f3f
Cloudomatic <cloudomatic@github.com>
2018-08-27 17:38:24 -0400

    Server side working in Python without image cleanup

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ A simple monitoring console for Foscam-compatible still and motion cameras (I ha
 
 1) Be able to store a large number of images (similar to how a hunting/game camera works) while selectively "thinning" the stored image folder (e.g. store hourly after one week, daily after 1 month, monthly after 1 year)
 
-2) Have a an HTML interface that will poll both motion (via the Foscam "snapshot" function) and still cameras into a multi-panel display while allowing one to (via a single click) go into the PTZ (pan-tilt-zoom) interface for that camera (if supported).
+2) Have a an HTML interface that will poll both motion (via the Foscam "snapshot" function) and still cameras into a multi-panel display while allowing one to (via a single click) go into the camera's built-in PTZ (pan-tilt-zoom) or other live interface.
 
--- bin/camera_scan_daemon.py
+++ bin/camera_scan_daemon.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-import datetime, time, os, sys, requests, shutil
+import datetime, time, os, sys, requests, shutil, json
 
-# e.g. ALL, ERROR, INFO, NONE
+# e.g. ALL, DEBUG, INFO, NONE 
 log_level='ALL'
 
 # This is the lsit of "known" cameras.  If IP addresses are not known, put in all "suspected" addresses.
@@ -39,39 +39,50 @@ number_of_still_images_to_retain=1000
 def run_image_cleanup():
   # TO DO:  This.  Note that we want to keep a certain number of images regardless of the polling interval.  If we have 1000 images, we should sporadically delete
   #         images so that we always have as long a history as possible, just "less" history (for example we go from one image an hour, to one per day, to one per month, etc)
-  log("run_image_cleanup(): >");
+  log("run_image_cleanup(): >", "ALL");
+  if not active cameras:
+    # Do nothing, as there's no reason to do any image cleanup.  
+  else:
+    # We want [number_of_still_images_to_retain] total, so to get the number of images to retain per camera:
+    number_of_still_images_to_retain_per_camera = number_of_still_images_to_retain / len(active_cameras)
+    #for camera in active_cameras:
+    #  number_of_still_images_currently_stored_for_this_camera = 
+    # returnnumber_of_still_images_to_retain
 
 def check_for_sumpple_camera_at_address(ip_address):
-  log("check_for_sumpple_camera_at_address(): > ip_address=" + ip_address);
+  log("check_for_sumpple_camera_at_address(): > ip_address=" + ip_address, "ALL");
   try: 
     camera_status_request = requests.get('http://' + ip_address + '/js/customer.js')
     log("check_for_sumpple_camera_at_address(): http status code: " + str(camera_status_request.status_code), "DEBUG");
     if (camera_status_request.status_code == 200):
-      log("check_for_sumpple_camera_at_address(): found camera at " + ip_address);
+      log("check_for_sumpple_camera_at_address(): found camera at " + ip_address, "DEBUG");
       return True
   except:
       pass
   return False
 
 def scan_network_for_cameras():
+  global last_scan_for_active_cameras, active_cameras
+  
   # TO DO: Instead of using a static list of IPs, accept a subnet mask and scan the subnet looking for cameras
-  log("scan_network_for_cameras(): >");
+  log("scan_network_for_cameras(): >", "ALL");
+  active_cameras=[]
   for camera_address in camera_list:
-    log("scan_network_for_cameras(): scanning camera at address: " + camera_address)
+    log("scan_network_for_cameras(): scanning camera at address: " + camera_address, "DEBUG")
     if (check_for_sumpple_camera_at_address(camera_address)):
       log("scan_network_for_cameras(): Found a compatible camera at " + camera_address)
       active_cameras.append(camera_address)
   last_scan_for_active_cameras = int(round(time.time() * 1000))
+  log("scan_network_for_cameras(): Setting last_scan_for_active_cameras to: " + str(last_scan_for_active_cameras), "DEBUG")
   
 def pull_still_images_from_active_cameras():
-  log("pull_still_images_from_active_cameras(): >")
+  log("pull_still_images_from_active_cameras(): >", "DEBUG")
   try:
     with open(camera_api_password_file, 'r') as password_file:
       camera_api_password = password_file.read().strip()
   except IOError:
     log ("pull_still_images_from_active_cameras(): !!!!! FATAL ERROR !!!!!!: Could not locate the password file (" + camera_api_password_file  + ")")
     sys.exit()
-  log("pull_still_images_from_active_cameras(): Using credentials (" + camera_api_username + "," + camera_api_password + ")")
   for camera_address in active_cameras:
     # We need two timestamps here, an "image overlay" timestamp, and a timestamp to put on the image file itself (for the function where the user wants to grab an archived image.  
     #   Some cameras will print the current timestamp on the jpeg itself,
@@ -81,22 +92,32 @@ def pull_still_images_from_active_cameras():
     current_time=datetime.datetime.now()
     image_file_timestamp='{:%Y.%m.%d_%H:%M:%S}'.format(current_time)
     image_display_timestamp='{:%Y.%m.%d %H:%M:%S}'.format(current_time)
-    log("pull_still_images_from_active_cameras(): Retrieving still image from " + camera_address + " via the FOSCAM 'snapshot' function", "DEBUG")
+    log("pull_still_images_from_active_cameras(): Retrieving still image from " + camera_address + " via the FOSCAM 'snapshot' function", "INFO")
     snapshot_request=requests.get("http://" + camera_address + "/cgi-bin/video_snapshot.cgi?user=" + camera_api_username + "&pwd=" + camera_api_password, stream=True)
     log("pull_still_images_from_active_cameras(): snapshot HTTP response code: " + str(snapshot_request.status_code), "DEBUG")
     if snapshot_request.status_code == 200:
       snapshot_filename=image_data_base_folder + "/" + camera_address + ".latest.jpg"
       snapshot_archived_filename=image_data_base_folder + "/" + camera_address + "_" + image_file_timestamp + ".jpg"
+      snapshot_metadata_filename=image_data_base_folder + "/" + camera_address + ".metadata"
+      snapshot_metadata='{ "timestamp" : "' + image_display_timestamp + '"}'
       with open(snapshot_archived_filename, 'wb') as snapshot_file:
         snapshot_request.raw.decode_content=True
         shutil.copyfileobj(snapshot_request.raw, snapshot_file)
       # We copy the archived file to the "latest" file after the download is complete so that we don't get a partial image
       shutil.copyfile(snapshot_archived_filename, snapshot_filename)
+      # Store the metadata
+      with open(snapshot_metadata_filename, 'w') as snapshot_metadata_file:
+        snapshot_metadata_file.write(snapshot_metadata)
+      
 
 def scan_loop():
-  log("scan_loop(): >")
+  log("scan_loop(): >", "ALL")
   while True: 
     current_time = int(round(time.time() * 1000))
+    log("scan_loop(): last_scan_for_active_cameras="  + str(last_scan_for_active_cameras)  + 
+                    " current_time=" + str(current_time) + 
+                    " scan_for_active_cameras_polling_interval_in_seconds=" + str(scan_for_active_cameras_polling_interval_in_seconds),  
+    "DEBUG")
     if (
          (last_scan_for_active_cameras == 0) or 
          ((current_time - last_scan_for_active_cameras) > scan_for_active_cameras_polling_interval_in_seconds)
@@ -114,7 +135,11 @@ def scan_loop():
 
 def log(message, level="INFO"):
   if (level >= log_level):
-    print "SimpleSumpple: " + '{:%Y.%m.%d %H:%M:%S}'.format(datetime.datetime.now()) + ": " + message
+    if level !=  "INFO":
+      log_level_label=": " +  level[0]
+    else:
+      log_level_label=""
+    print "SimpleSumpple: " + '{:%Y.%m.%d %H:%M:%S}'.format(datetime.datetime.now()) + log_level_label + ": " + message
 
 if __name__ == "__main__":
   scan_loop()
